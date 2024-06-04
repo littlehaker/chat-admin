bedce78aba198d2ac409f729582cc04b1077d658
Uno Young <littlehaker@gmail.com>
2024-06-04 16:01:03 +0800

    feat: use string to generate tableConfig

--- app/context/config-context.ts
+++ app/context/config-context.ts
@@ -1,27 +1,31 @@
 import { createContext } from "react";
 import { admin } from "../dsl/admin-dsl";
 
-export const tableConfig = admin((t) => {
-  t.pagination([2, 10, 20]);
+function buildConfig(code: string) {
+  return admin(new Function("t", code));
+}
 
-  t.field("id");
-  t.field("title", { filterable: true });
-  t.enumField(
-    "priority",
-    [
-      { label: "High", value: "high" },
-      { label: "Medium", value: "medium", variant: "secondary" },
-    ],
-    { filterable: true }
-  );
+export const tableConfig = buildConfig(`
+t.pagination([2, 10, 20]);
 
-  t.rowAction("Edit");
-  t.rowAction("Delete", { confirmText: "Want to delete?" }, (row) => {
-    console.log("Deleting row:", row);
-  });
+t.field("id");
+t.field("title", { filterable: true });
+t.enumField(
+  "priority",
+  [
+    { label: "High", value: "high" },
+    { label: "Medium", value: "medium", variant: "secondary" },
+  ],
+  { filterable: true }
+);
 
-  t.bulkAction("Delete");
-  t.bulkAction("Download");
+t.rowAction("Edit");
+t.rowAction("Delete", { confirmText: "Want to delete?" }, (row) => {
+  console.log("Deleting row:", row);
 });
+
+t.bulkAction("Delete");
+t.bulkAction("Download");
+`);
 
 export const ConfigContext = createContext(tableConfig);
